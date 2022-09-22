fdeccf48c323dd5d0e72c9d5507f5d0faaa65c0f
Heng Liu <liu.heng@microsoft.com>
2022-09-21 22:20:36 -0700

    address feedback: Refactoring

---
+++ src/SharedUtility/TestHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace SharedUtility
+{
+    public class TestHelpers
+    {
+        public static string GetResource(string name, Type type)
+        {
+            ArgumentNullException.ThrowIfNull(name);
+            ArgumentNullException.ThrowIfNull(type);
+            // var manifestResourceStream = type.GetTypeInfo().Assembly.GetManifestResourceStream(name);
+            var testtype = type.GetTypeInfo();
+            var testassembly = testtype.Assembly;
+            var manifestResourceStream = testassembly.GetManifestResourceStream(name);
+
+            if (manifestResourceStream != null)
+            {
+                using StreamReader reader = new StreamReader(manifestResourceStream);
+                return reader.ReadToEnd();
+            }
+            else
+            {
+                throw new ArgumentException($"Cannot find the resource {name}");
+            }
+        }
+    }
+}
