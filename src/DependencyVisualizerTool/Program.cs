9866c281f380a1b512dfc441e70c6ec9dcfb8ee1
Heng Liu <liu.heng@microsoft.com>
2022-09-23 00:26:48 -0700

    work

--- src/DependencyVisualizerTool/Program.cs
+++ src/DependencyVisualizerTool/Program.cs
@@ -5,50 +5,135 @@ using Logging;
 using Microsoft.Extensions.Logging;
 using static DependencyVisualizerTool.MSBuildUtility;
 using NuGet.ProjectModel;
+using static NuGet.Packaging.PackagingConstants;
+using Newtonsoft.Json;
+using System.Globalization;
+using Common;
 
-MSBuildLocator.RegisterDefaults();
-
-var fileArgument = new Argument<FileInfo?>(
-    name: "projectFilePath",
-    description: "path to the project file",
-    parse: result =>
+namespace DependencyVisualizerTool
+{
+    internal class Program
     {
-        string? filePath = result.Tokens.Single().Value;
-        if (!File.Exists(filePath))
+        public static int Main(string[] args)
         {
-            result.ErrorMessage = "Project file does not exists";
-            AppLogger.Logger.LogError("Project file does not exists");
-            return null;
+            MSBuildLocator.RegisterDefaults();
+
+            var fileArgument = new Argument<FileInfo>(
+                name: "projectFilePath",
+                description: "path to the project file to be used for finding the package.assets.file",
+                parse: result =>
+                {
+                    string? filePath = result.Tokens.Single().Value;
+                    if (!File.Exists(filePath))
+                    {
+                        result.ErrorMessage = "Invalid argument projectFilePath. The path does not exists.";
+                        AppLogger.Logger.LogError(result.ErrorMessage);
+                        throw new ArgumentException(result.ErrorMessage);
+                    }
+                    return new FileInfo(filePath);
+                });
+
+            var outputOption = new Option<string?>(
+                name: "--outputFolder",
+                description: "A folder path used to store generated DGML files. By default, it's the project folder.");
+
+            var rootCommand = new RootCommand("Dependency visualizer app");
+            rootCommand.AddArgument(fileArgument);
+            rootCommand.AddOption(outputOption);
+
+            rootCommand.SetHandler((fileArgument, outputOption) =>
+            {
+                GenerateGraph(fileArgument, outputOption);
+            },
+            fileArgument, outputOption);
+
+            return rootCommand.InvokeAsync(args).Result;
         }
-        return new FileInfo(filePath);
-    });
 
-var rootCommand = new RootCommand("Dependency visualizer app for System.CommandLine");
-
-rootCommand.AddArgument(fileArgument);
-rootCommand.SetHandler((file) =>
-{
-    ReadFile(file!);
-},
-    fileArgument);
-
-return rootCommand.InvokeAsync(args).Result;
-
-static void ReadFile(
-            FileInfo file)
-{
-    try
-    {
-        string assetsFilePath = Path.Combine(GetMSBuildProjectExtensionsPath(file.FullName), LockFileFormat.AssetsFileName);
-        List<string> lines = File.ReadLines(assetsFilePath).ToList();
-        foreach (string line in lines)
+        private static void GenerateGraph(FileInfo projectFile, string? outputFolder)
         {
-            Console.WriteLine(line);
-        };
+            ArgumentNullException.ThrowIfNull(projectFile);
+            string projectExtensionsPath = GetMSBuildProjectExtensionsPath(projectFile.FullName);
+            LockFile assetFile = GetAssetsFilePath(projectExtensionsPath);
+            DependencyGraphSpec dgspecFile = GetDgspecFilePath(projectExtensionsPath, projectFile);
+   
+            if (outputFolder == null)
+            {
+                outputFolder = Path.GetDirectoryName(projectFile.FullName);
+            }
+            else
+            {
+                CreateOutputIfNotExist(outputFolder);
+            }
+
+            Dictionary<string, PackageDependencyGraph> dictGraph = PackageDependencyGraph.GenerateAllDependencyGraphsFromAssetsFile(assetFile, dgspecFile);
+            foreach (var keyValuePair in dictGraph)
+            {
+                string projectName = Path.GetFileNameWithoutExtension(projectFile.Name);
+                string tfm = keyValuePair.Key;
+                string dgmlFileName = Path.Combine(outputFolder, $"{projectName}_{tfm}.dgml");
+                DGMLDependencyVisualizerTool.TransGraphToDGMLFile(keyValuePair.Value, dgmlFileName);
+            }
+        }
+        private static LockFile GetAssetsFilePath(string projectExtensionsPath)
+        {
+            ArgumentNullException.ThrowIfNull(projectExtensionsPath);
+            try
+            {
+                string assetsFilePath = Path.Combine(projectExtensionsPath, LockFileFormat.AssetsFileName);
+                if (!File.Exists(assetsFilePath))
+                {
+                    AppLogger.Logger.LogError($"Assets file: {assetsFilePath} doesn't exist. Please make sure restore is done on this project before running this command.");
+                }
+                LockFile assetsFile = new LockFileFormat().Read(assetsFilePath);
+                return assetsFile;
+            }
+            catch (Exception e)
+            {
+                AppLogger.Logger.LogError($"Failed to read the assets file: \n {e.Message}");
+                throw;
+            }
+
+        }
+        private static DependencyGraphSpec GetDgspecFilePath(string projectExtensionsPath, FileInfo projectFile)
+        {
+            ArgumentNullException.ThrowIfNull(projectExtensionsPath);
+            ArgumentNullException.ThrowIfNull(projectFile);
+            try
+            {
+                string dgspecFileName = DependencyGraphSpec.GetDGSpecFileName(projectFile.Name);
+                string dgspecFileFullPath = Path.Combine(projectExtensionsPath, dgspecFileName);
+                if (!File.Exists(dgspecFileFullPath))
+                {
+                    AppLogger.Logger.LogError($"Dgspec file: {dgspecFileFullPath} doesn't exist. Please make sure restore is done on this project before running this command.");
+                }
+                DependencyGraphSpec dgspecFile = DependencyGraphSpec.Load(dgspecFileFullPath);
+                return dgspecFile;
+            }
+            catch (Exception e)
+            {
+                AppLogger.Logger.LogError(e.Message);
+                throw;
+            }
+        }
+
+        private static void CreateOutputIfNotExist(string outputFolder)
+        {
+            if (!Directory.Exists(outputFolder))
+            {
+                try
+                {
+                    Directory.CreateDirectory(outputFolder);
+                }
+                catch (Exception e)
+                {
+                    var errorMessage = $"Failed in creating output folder. \n{e.Message}";
+                    AppLogger.Logger.LogError(errorMessage);
+                    throw;
+                }
+            }
+        }
+
     }
-    catch (Exception e)
-    {
-        AppLogger.Logger.LogError(e.Message);
-        throw;
-    }
+
 }
